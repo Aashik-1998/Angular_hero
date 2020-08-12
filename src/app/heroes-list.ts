db87976baa841bb9dabbbf6a9b310afcfd2730ec
Aashik-1998 <aashikmazz67@gmail.com>
2020-08-12 11:04:03 +0530

    Hero task

---
+++ src/app/heroes-list.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Iron Man' },
+  { id: 2, name: 'Captain America' },
+  { id: 3, name: 'Thor' },
+  { id: 4, name: 'Hulk' },
+  { id: 5, name: 'Natasha' },
+  { id: 6, name: 'Ant-Man' },
+  { id: 7, name: 'Dr Strange' },
+  { id: 8, name: 'Spider Man' },
+  { id: 9, name: 'Black Panther' },
+  { id: 10, name: 'Captain Marvel' }
+];
